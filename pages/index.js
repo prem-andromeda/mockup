30f1ae5e029d0f8ae02174273d6d2165fa349420
prem-andromeda <prem.narayankar@andromeda.in>
2022-11-16 14:30:58 +0530

    Image component added

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <nav className="navbar bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              <img src="logo.svg" alt="" />
+              <Image src="logo.svg" alt="" />
             </a>
           </div>
         </nav>
